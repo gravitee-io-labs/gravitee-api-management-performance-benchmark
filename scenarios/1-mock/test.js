b953be6ba1b27957a3903679a64c2bf99ea05795
Dorian BLANC <dorian.blanc@graviteesource.com>
2024-03-25 01:53:53 +0100

    Increase from 200 rps to 2000 rps

--- scenarios/1-mock/test.js
+++ scenarios/1-mock/test.js
@@ -2,17 +2,17 @@ import http from 'k6/http';
 
 export const options = {
   scenarios: {
-    "50rps-30s": {
+    "2000rps-30s": {
       executor: 'constant-arrival-rate',
       duration: '30s',
       preAllocatedVUs: 50,
       maxVUs: 100,
-      rate: 200,
+      rate: 2000,
       timeUnit: '1s',
     },
   },
 };
 
 export default function () {
-  http.get('http://apim-gateway.gio-apim.svc.cluster.local:82/echo');
+  http.get('http://apim-gateway.gio-apim.svc.cluster.local:82/mock');
 }
