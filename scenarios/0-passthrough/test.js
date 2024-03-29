b4cf957151466769b113f771f16a3ffdf1a37e43
Dorian BLANC <dorian.blanc@graviteesource.com>
2024-03-29 18:14:06 +0100

    Update Passthrough scenario

--- scenarios/0-passthrough/test.js
+++ scenarios/0-passthrough/test.js
@@ -2,17 +2,32 @@ import http from 'k6/http';
 
 export const options = {
   scenarios: {
-    "50rps-30s": {
+    "warm-up": {
       executor: 'constant-arrival-rate',
       duration: '30s',
-      preAllocatedVUs: 50,
-      maxVUs: 100,
+      gracefulStop: '10s',
+      preAllocatedVUs: 1000,
       rate: 200,
       timeUnit: '1s',
     },
-  },
+    "passthrough": {
+      executor: 'ramping-arrival-rate',
+      startTime: '50s', // 10s to 20s after warm-up
+      gracefulStop: '10s',
+      preAllocatedVUs: 20000,
+      maxVUs: 50000,
+      startRate: 200,
+      timeUnit: '1s',
+      stages: [
+        { target: 200, duration: '10s' },
+        { target: 25000, duration: '3m' },
+        { target: 25000, duration: '30s' },
+        { target: 200, duration: '30s' }
+      ]
+    }
+  }
 };
 
 export default function () {
-  http.get('http://apim-gateway.gio-apim.svc.cluster.local:82/echo');
+  http.get('http://apim-gateway.gio-apim.svc.cluster.local:82/passthrough/json/valid');
 }
