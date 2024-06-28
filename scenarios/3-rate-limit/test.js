f7919f59b4736f932854aa471d00a43c27dd07bd
Dorian BLANC <dorian.blanc@graviteesource.com>
2024-06-28 02:09:12 +0200

    Update Rate Limit Scenario

---
+++ scenarios/3-rate-limit/test.js
@@ -0,0 +1,43 @@
+import { check } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+  scenarios: {
+    "warm-up": {
+      executor: 'constant-arrival-rate',
+      duration: '30s',
+      gracefulStop: '10s',
+      preAllocatedVUs: 10,
+      maxVUs: 10,
+      rate: 200,
+      timeUnit: '1s',
+    },
+    "rate-limit": {
+      executor: 'ramping-arrival-rate',
+      startTime: '40s', // 10s after warm-up
+      gracefulStop: '10s',
+      preAllocatedVUs: 100,
+      maxVUs: 100,
+      startRate: 200,
+      timeUnit: '1s',
+      stages: [
+        { target: 10000, duration: '2m' }, // Configure your limit here
+        { target: 10000, duration: '10s' }, // Configure your limit here
+        { target: 200, duration: '30s' },
+        { target: 200, duration: '30s' }
+      ]
+    },
+  },
+};
+
+http.setResponseCallback(
+  http.expectedStatuses(200, 429)
+);
+
+export default function () {
+  const res = http.get('http://apim-gateway.gio-apim.svc.cluster.local:82/ratelimit/json/valid');
+  // check(res, {
+  //   'Success with status 200': (r) => r.status === 200,
+  //   'Under rate limit with status 429': (r) => r.status === 429,
+  // });
+}
